15610e6b38de37a937088fc014094a6baba03211
Stephen <Stephen@192.168.0.11>
2016-12-04 13:06:47 -0500

    Epsilon approximations for double comparisons

--- DistributedFinalProject/src/robotevac/EvacCircle.java
+++ DistributedFinalProject/src/robotevac/EvacCircle.java
@@ -1,53 +1,50 @@
 package robotevac;
 
+import static robotevac.EvacPoint.EPSILON;
+
+import java.util.Random;
+
 public class EvacCircle {
 	private EvacPoint 	exit;
-	private int 		radius = 1;
-	
+	private int radius = 1;
+
 	public EvacCircle(double ex, double ey) {
 		exit = new EvacPoint(ex, ey);
 	}
-	
+
 	public EvacCircle(EvacPoint e) {
 		exit = e;
 	}
-	
+
 	public EvacCircle() {
-		double x = 0;
-		double y = 0;
-		while (true) {
-			x = 2 * Math.random() - 1;
-			y = 2 * Math.random() - 1;
-			EvacPoint e = new EvacPoint(x, y);
-			if (isInside(e)) {
-				exit = e;
-				break;
-			}
-		}
+		Random r = new Random();
+		double rads = Math.toRadians(r.nextInt(360));
+		exit = new EvacPoint(radius * Math.sin(rads), radius * Math.cos(rads));
 	}
-	
+
 	public EvacPoint getExit() {
 		return exit;
 	}
-	
+
 	public double getRadius() {
 		return radius;
 	}
-	
+
 	public boolean isExit(EvacPoint e) {
 		return exit.equals(e);
 	}
-	
+
 	public boolean isExit(double x, double y) {
 		return exit.equals(new EvacPoint(x, y));
 	}
-	
+
 	public static boolean isInside(EvacPoint p) {
-		return ((p.getX() * p.getX()) + (p.getY() * p.getY()) <= 1);
+		return ((p.getX() * p.getX()) + (p.getY() * p.getY()) <= 1 + EPSILON);
 	}
-	
+
 	public static boolean isOnCircumference(EvacPoint p) {
-		return ((p.getX() * p.getX()) + (p.getY() * p.getY()) == 1);
+		double dist = (p.getX() * p.getX()) + (p.getY() * p.getY());
+		return (dist + EPSILON >= 1 && dist - EPSILON <= 1);
 	}
 
 }
--- DistributedFinalProject/src/robotevac/EvacPoint.java
+++ DistributedFinalProject/src/robotevac/EvacPoint.java
@@ -1,23 +1,25 @@
 package robotevac;
 
 public class EvacPoint {
+	public static final double EPSILON = 0.000001;
 	private double x;
 	private double y;
-	
+
 	public EvacPoint(double xLoc, double yLoc) {
 		x = xLoc;
 		y = yLoc;
 	}
-	
+
 	public double getX() {
 		return x;
 	}
-	
+
 	public double getY() {
 		return y;
 	}
-	
+
 	public boolean equals(EvacPoint p) {
-		return x == p.getX() && y == p.getY();
+		return x + EPSILON >= p.getX() && x - EPSILON <= p.getX()
+				&&  y + EPSILON >= p.getY() && y - EPSILON <= p.getY();
 	}
 }
