1389dc88e0a45a41b1d6cacd50e8157b36089aab
laptop <laptop@192.168.0.13>
2016-12-11 17:40:39 -0500

    Added the code to find the destination for two random bots

--- DistributedFinalProject/src/robotevac/MainControl.java
+++ DistributedFinalProject/src/robotevac/MainControl.java
@@ -1,6 +1,7 @@
 package robotevac;
 
 import display.GUIView;
+import static robotevac.EvacPoint.EPSILON;
 
 public class MainControl {
 	private GUIView view = new GUIView();
@@ -71,33 +72,76 @@ public class MainControl {
 			case BOTH_RANDOM:
 				// TODO find worst case
 				circle = new EvacCircle(new EvacPoint(Math.sin((2*Math.PI)/3),
-						Math.cos(Math.sin((2*Math.PI)/3))));
+						Math.cos((2*Math.PI)/3)));
 				break;
 			}
 			break;
 		}
+	}
+	//finds the closest point to both robots when both robots are placed randomly
+	private EvacPoint findDestination() {
+		//find the angles at which the radius crosses the circumference through
+		//the points
+		double angle1 = EvacCircle.getAngle(robot1.getLocation().getX(),
+				robot1.getLocation().getY());
+		double angle2 = EvacCircle.getAngle(robot2.getLocation().getX(),
+				robot2.getLocation().getY());
+		//use those angles to find the points on the circle where the radius crosses
+		EvacPoint p1 = new EvacPoint(Math.sin(angle1), Math.cos(angle1));
+		EvacPoint p2 = new EvacPoint(Math.sin(angle2), Math.cos(angle2));
+		//find the distance from each of the robots to those points
+		double deltaX1 = p1.getX() - robot1.getLocation().getX();
+		double deltaY1 = p1.getY() - robot1.getLocation().getY();
+		double d1 = Math.sqrt(deltaX1 * deltaX1 + deltaY1 * deltaY1);
+		double deltaX2 = p2.getX() - robot2.getLocation().getX();
+		double deltaY2 = p2.getY() - robot2.getLocation().getY();
+		double d2 = Math.sqrt(deltaX2 * deltaX2 + deltaY2 * deltaY2);
+		//get the ratio of those distances
+		double ratio = d1 / d2;
+		//get the angles of the points on the circle
+		double loc1 = EvacCircle.getAngle(p1.getX(), p1.getY());
+		double loc2 = EvacCircle.getAngle(p2.getX(), p2.getY());
+		//if both robots are equidistant from the circumference then the destination
+		//would be halfway between the closest points to the circle for each of them
+		//therefore if we take that distance and multiply it by the ratio between
+		//the distances to the circle, it should give the point where they travel the same
+		//distance to meet
+		double distance = Math.abs(loc1 - loc2) / 2 * ratio;
+		//find which way we need to rotate and rotate our destination appropriately
+		double location;
+		if (loc1 - EPSILON < loc2) {
+			location = loc1 + distance;
+		}
+		else {
+			location = loc1 - distance;
+		}
+		//find what point that would be and make it the destination
+		return new EvacPoint(Math.sin(location), Math.cos(location));
 	}
 
 	//the main loop that makes the robots move along the path to the exit
 	private double runAlgorithm() {
 		//figure out where the robots need to go to on the circumference
 		switch (settings.getRobotMode()) {
+		//if they're both in the center any point will do
 		case BOTH_CENTER:
-			//if they're both in the center any point will do
 			robot1.setDestination(new EvacPoint(0,1));
 			robot2.setDestination(new EvacPoint(0,1));
 			break;
+		//if one is random must find the point on the circumference along the line
+		//that the two robots are on
 		case ONE_RANDOM:
-			//if one is random must find the point on the circumference along the line
-			//that the two robots are on
-			double angle = EvacCircle.getAngle(robot2.getLocation().getX(), robot2.getLocation().getY());
+			double angle = EvacCircle.getAngle(robot2.getLocation().getX(),
+					robot2.getLocation().getY());
 			robot1.setDestination(new EvacPoint(Math.sin(angle), Math.cos(angle)));
 			robot2.setDestination(new EvacPoint(Math.sin(angle), Math.cos(angle)));
 			break;
+		//if both are random must find the point on the circumference such that the maximum
+		//of the two robots' distances to said point is minimized
 		case BOTH_RANDOM:
-			//if both are random must find the point on the circumference such that the maximum
-			//of the two robots' distances to said point is minimized
-			// TODO figure out destination
+			EvacPoint destination = findDestination();
+			robot1.setDestination(destination);
+			robot2.setDestination(destination);
 			break;
 		}
 		view.startSimulation(robot1, robot2, circle);
