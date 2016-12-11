88b7d03e5a88cf3c5b88802fcd838daba59c2c9d
laptop <laptop@192.168.0.14>
2016-12-10 22:34:15 -0500

    Added comments to Robot and MainControl

--- DistributedFinalProject/src/robotevac/MainControl.java
+++ DistributedFinalProject/src/robotevac/MainControl.java
@@ -11,16 +11,21 @@ public class MainControl {
 	private Robot 		robot2;
 	private EvacCircle 	circle;
 
+	//initialize the robots based on robot mode chosen by user
+	//robot1 always goes clockwise while robot2 always goes counter-clockwise
 	private void initRobots() {
 		switch (settings.getRobotMode()) {
+		//both start in the center
 		case BOTH_CENTER:
 			robot1 = new Robot(0, 0, Direction.CW);
 			robot2 = new Robot(0, 0, Direction.CCW);
 			break;
+		//robot1 starts in the center, robot2 starts at a random spot in the circle
 		case ONE_RANDOM:
 			robot1 = new Robot(0, 0, Direction.CW);
 			robot2 = new Robot(EvacCircle.randomPointInside(), Direction.CCW);
 			break;
+		//both robots start at random spots in the circle
 		case BOTH_RANDOM:
 			robot1 = new Robot(EvacCircle.randomPointInside(), Direction.CW);
 			robot2 = new Robot(EvacCircle.randomPointInside(), Direction.CCW);
@@ -28,18 +33,22 @@ public class MainControl {
 		}
 	}
 
+	//initialize the circle based on the user's choice for exit
 	private void initCircle() {
 		switch (settings.getExitMode()) {
+		//default constructor will give a circle with a random exit, so use that
 		case RANDOM:
 			circle = new EvacCircle();
 			break;
+		//worst case changes based on where robots start
 		case WORST_CASE:
 			switch (settings.getRobotMode()) {
+			//if both start at center worst case is fixed
 			case BOTH_CENTER:
 				circle = new EvacCircle(new EvacPoint(Math.sin((2*Math.PI)/3),
 						Math.cos((2*Math.PI)/3)));
 				break;
-
+			//if one robot is random need to find worst case
 			case ONE_RANDOM:
 				double x = robot2.getLocation().getX();
 				double y = robot2.getLocation().getY();
@@ -63,7 +72,7 @@ public class MainControl {
 					System.out.println("Worst case time: " + total2);
 				}
 				break;
-
+			//if both robots are random also need to find worst case
 			case BOTH_RANDOM:
 				// TODO find worst case
 				circle = new EvacCircle(new EvacPoint(Math.sin((2*Math.PI)/3),
@@ -74,21 +83,29 @@ public class MainControl {
 		}
 	}
 
+	//the main loop that makes the robots move along the path to the exit
 	private double runAlgorithm() {
+		//figure out where the robots need to go to on the circumference
 		switch (settings.getRobotMode()) {
 		case BOTH_CENTER:
+			//if they're both in the center any point will do
 			robot1.setDestination(new EvacPoint(0,1));
 			robot2.setDestination(new EvacPoint(0,1));
 			break;
 		case ONE_RANDOM:
+			//if one is random must find the point on the circumference along the line
+			//that the two robots are on
 			double angle = EvacCircle.getAngle(robot2.getLocation().getX(), robot2.getLocation().getY());
 			robot1.setDestination(new EvacPoint(Math.sin(angle), Math.cos(angle)));
 			robot2.setDestination(new EvacPoint(Math.sin(angle), Math.cos(angle)));
 			break;
 		case BOTH_RANDOM:
+			//if both are random must find the point on the circumference such that the maximum
+			//of the two robots' distances to said point is minimized
 			// TODO figure out destination
 			break;
 		}
+		//keep moving the robots until both have exited
 		while (!robot1.atExit() || !robot2.atExit()) {
 			EvacPoint prevLoc1 = robot1.getLocation();
 			EvacPoint prevLoc2 = robot2.getLocation();
@@ -96,67 +113,102 @@ public class MainControl {
 			EvacPoint prevDest2 = robot2.getDestination();
 			MoveMode prevMode1 = robot1.getMode();
 			MoveMode prevMode2 = robot2.getMode();
+			//move the robots
 			if (!robot1.atExit()) robot1.move();
 			if (!robot2.atExit()) robot2.move();
+			//check if the first robot hit the exit while transitioning between moving to the
+			//circumference and rotating around the circle
 			if (prevMode1.equals(MoveMode.CIRCUMFERENCE)
 					&& robot1.getMode().equals(MoveMode.ROTATE)) {
-				if (circle.exitBetween(prevDest1, robot1.getLocation(), robot1.getDirection())) {
+				if (circle.exitBetween(
+						prevDest1, robot1.getLocation(), robot1.getDirection())) {
+					//find the distance that the first robot overshot the exit by
 					double angle1 = EvacCircle.getAngle(circle.getExit().getX(),
 							circle.getExit().getY());
 					double angle2 = EvacCircle.getAngle(robot1.getLocation().getX(),
 							robot1.getLocation().getY());
 					double d = Math.abs(angle1 - angle2);
+					//reverse the first robot by the overshoot distance and set it to exited
 					robot1.reverse(d);
 					robot1.exit();
+					//also set it to exit mode so it doesn't trigger any of these cases
 					robot1.setMode(MoveMode.EXIT);
+					//reverse the second robot by the overshoot distance, set it to exit mode,
+					//give it the exit as a destination and move it towards the exit by the
+					//overshoot distance
 					robot2.reverse(d);
 					robot2.setDestination(circle.getExit());
 					robot2.setMode(MoveMode.EXIT);
 					robot2.moveStraight(d);
 				}
 			}
+			//check if the second robot hit the exit while transitioning between moving to the
+			//circumference and rotating around the circle
 			else if (prevMode2.equals(MoveMode.CIRCUMFERENCE)
 					&& robot2.getMode().equals(MoveMode.ROTATE)) {
-				if (circle.exitBetween(prevDest2, robot2.getLocation(), robot2.getDirection())) {
+				if (circle.exitBetween(
+						prevDest2, robot2.getLocation(), robot2.getDirection())) {
+					//find the distance that the second robot overshot the exit by
 					double angle1 = EvacCircle.getAngle(circle.getExit().getX(),
 							circle.getExit().getY());
 					double angle2 = EvacCircle.getAngle(robot2.getLocation().getX(),
 							robot2.getLocation().getY());
 					double d = Math.abs(angle1 - angle2);
+					//reverse the second robot by the overshoot distance and set it to exited
 					robot2.reverse(d);
 					robot2.exit();
+					//also set it to exit mode so it doesn't trigger any of these cases
 					robot2.setMode(MoveMode.EXIT);
+					//reverse the first robot by the overshoot distance, set it to exit mode,
+					//give it the exit as a destination and move it towards the exit by the
+					//overshoot distance
 					robot1.reverse(d);
 					robot1.setDestination(circle.getExit());
 					robot1.setMode(MoveMode.EXIT);
 					robot1.moveStraight(d);
 				}
 			}
+			//check if the first robot hit the exit while rotating around the circle
 			if (robot1.getMode().equals(MoveMode.ROTATE)
-					&& circle.exitBetween(prevLoc1, robot1.getLocation(), robot1.getDirection())) {
+					&& circle.exitBetween(
+							prevLoc1, robot1.getLocation(), robot1.getDirection())) {
+				//find the distance that the first robot overshot the exit by
 				double angle1 = EvacCircle.getAngle(circle.getExit().getX(),
 						circle.getExit().getY());
 				double angle2 = EvacCircle.getAngle(robot1.getLocation().getX(),
 						robot1.getLocation().getY());
 				double d = Math.abs(angle1 - angle2);
+				//reverse the first robot by the overshoot distance and set it to exited
 				robot1.reverse(d);
 				robot1.exit();
+				//also set it to exit mode so it doesn't trigger any of these cases
 				robot1.setMode(MoveMode.EXIT);
+				//reverse the second robot by the overshoot distance, set it to exit mode,
+				//give it the exit as a destination and move it towards the exit by the
+				//overshoot distance
 				robot2.reverse(d);
 				robot2.setDestination(circle.getExit());
 				robot2.setMode(MoveMode.EXIT);
 				robot2.moveStraight(d);
 			}
-			if (robot2.getMode().equals(MoveMode.ROTATE)
-					&& circle.exitBetween(prevLoc2, robot2.getLocation(), robot2.getDirection())) {
+			//check if the second robot hit the exit while rotating around the circle
+			else if (robot2.getMode().equals(MoveMode.ROTATE)
+					&& circle.exitBetween(
+							prevLoc2, robot2.getLocation(), robot2.getDirection())) {
+				//find the distance that the first robot overshot the exit by
 				double angle1 = EvacCircle.getAngle(circle.getExit().getX(),
 						circle.getExit().getY());
 				double angle2 = EvacCircle.getAngle(robot2.getLocation().getX(),
 						robot2.getLocation().getY());
 				double d = Math.abs(angle1 - angle2);
+				//reverse the second robot by the overshoot distance and set it to exited
 				robot2.reverse(d);
 				robot2.exit();
+				//also set it to exit mode so it doesn't trigger any of these cases
 				robot2.setMode(MoveMode.EXIT);
+				//reverse the first robot by the overshoot distance, set it to exit mode,
+				//give it the exit as a destination and move it towards the exit by the
+				//overshoot distance
 				robot1.reverse(d);
 				robot1.setDestination(circle.getExit());
 				robot1.setMode(MoveMode.EXIT);
@@ -166,14 +218,18 @@ public class MainControl {
 		return Math.max(robot1.getDistance(), robot2.getDistance());
 	}
 
+	//main program, tells the view to get the parameters for the test and then uses the
+	//parameters to initialize the robots and circles, then run the algorithm accordingly
 	public void run() {
 		while (true) {
+			//get the settings from the user
 			settings = new SimulationSettings();
 			settings = view.getSimulationSettings();
 			switch (settings.getExitMode()) {
 			case RANDOM:
-				// TODO get number of experiments
-				int num = 20;
+				//get number of tests given by user, run that many tests
+				//reinitialize robots and circle each time, then give average time
+				int num = settings.getNumberOfTests();
 				double sum = 0;
 				for (int i = 0; i < num; i++) {
 					// TODO do we draw each experiment?
@@ -185,6 +241,8 @@ public class MainControl {
 				System.out.println("Average time: " + sum / num);
 				break;
 			case WORST_CASE:
+				//initialize the robots and circle just once, then run
+				//one test and give that worst case time
 				initRobots();
 				initCircle();
 				double time = runAlgorithm();
--- DistributedFinalProject/src/robotevac/Robot.java
+++ DistributedFinalProject/src/robotevac/Robot.java
@@ -47,52 +47,82 @@ public class Robot {
 		return direction;
 	}
 	
+	//move the robot by SPEED
+	//movement depends on current movement mode
+	//handles transition from circumference to rotate
+	//control handles transition from rotate to exit
 	public void move() {
 		switch(mode) {
 		case CIRCUMFERENCE:
+			//get the point that is SPEED along our current line
 			double deltaX = destination.getX() - location.getX();
 			double deltaY = destination.getY() - location.getY();
 			double angle = EvacCircle.getAngle(deltaX, deltaY);
 			double newX = location.getX() + Math.sin(angle) * SPEED;
 			double newY = location.getY() + Math.cos(angle) * SPEED;
 			EvacPoint newLoc = new EvacPoint(newX, newY);
+			//if it's not inside the circle we overshot our destination
 			if (!EvacCircle.isInside(newLoc)) {
+				//find out how much we overshot by
 				deltaX = newLoc.getX() - destination.getX();
 				deltaY = newLoc.getY() - destination.getY(); 
 				double d = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+				//increase total distance traveled by the distance between our previous
+				//location and our destination
 				distance += SPEED - d;
+				//set our location to our destination as we have arrived there
 				location = destination;
+				//set movement mode to rotate for future
 				mode = MoveMode.ROTATE;
+				//rotate around the circle by the overshoot distance
 				moveRotate(d);
 			}
 			else {
+				//we haven't hit our destination yet so our location just becomes the new
+				//location and we add SPEED to our total distance
 				location = newLoc;
 				distance += SPEED;
 			}
 			break;
 		case ROTATE:
+			//find our current position on the circle in radians
 			angle = EvacCircle.getAngle(location.getX(), location.getY());
+			//if we're going clockwise we need to add SPEED to our angle
+			//otherwise we subtract it from our angle
 			if (direction.equals(Direction.CW)) angle += SPEED;
 			else angle -= SPEED;
+			//adjust our angle if it goes negative
 			if (angle - EPSILON < 0) angle = 2 * Math.PI + angle;
+			//find our new position based on our angle and add SPEED to total distance
 			location = new EvacPoint(Math.sin(angle), Math.cos(angle));
 			distance += SPEED;
+			//remember that control takes care of exit transition
 			break;
 		case EXIT:
+			//get the point that is SPEED along our current line
 			deltaX = destination.getX() - location.getX();
 			deltaY = destination.getY() - location.getY();
 			angle = EvacCircle.getAngle(deltaX, deltaY);
 			newX = location.getX() + Math.sin(angle) * SPEED;
 			newY = location.getY() + Math.cos(angle) * SPEED;
 			newLoc = new EvacPoint(newX, newY);
+			//if it's not inside the circle we overshot our destination
 			if (!EvacCircle.isInside(newLoc)) {
+				//find out how much we overshot by
 				deltaX = newLoc.getX() - destination.getX();
 				deltaY = newLoc.getY() - destination.getY(); 
 				double d = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+				//set our location to our destination as we have arrived there
+				location = destination;
+				//increase total distance traveled by the distance between our previous
+				//location and our destination
 				distance += SPEED - d;
+				//set exited to true as we have found the exit
 				exited = true;
 			}
 			else {
+				//we haven't hit our destination yet so our location just becomes the new
+				//location and we add SPEED to our total distance
 				location = newLoc;
 				distance += SPEED;
 			}
@@ -100,43 +130,64 @@ public class Robot {
 		}
 	}
 	
+	//rotate along the circumference of the circle by d units
 	public void moveRotate(double d) {
+		//find our current position on the circle in radians
 		double angle = EvacCircle.getAngle(location.getX(), location.getY());
+		//if we're going clockwise we need to add d to our angle
+		//otherwise we subtract it from our angle
 		if (direction.equals(Direction.CW)) angle += d;
 		else angle -= d;
+		//adjust our angle if it goes negative
 		if (angle - EPSILON < 0) angle = 2 * Math.PI + angle;
+		//find our new position based on our angle and add d to total distance
 		location = new EvacPoint(Math.sin(angle), Math.cos(angle));
 		distance += d;
 	}
 	
 	public void moveStraight(double d) {
+		//get the point that is d along our current line
 		double deltaX = destination.getX() - location.getX();
 		double deltaY = destination.getY() - location.getY();
 		double angle = EvacCircle.getAngle(deltaX, deltaY);
 		double newX = location.getX() + Math.sin(angle) * d;
 		double newY = location.getY() + Math.cos(angle) * d;
+		//set location to the new location
 		location = new EvacPoint(newX, newY);
+		//increase total distance traveled by d
 		distance += d;
 	}
 
+	//move backwards by d units
+	//movement is changed depending on movement mode
 	public void reverse(double d) {
 		switch(mode) {
+		//if movement mode is rotate we need to move around the circle
 		case ROTATE:
+			//find our current position on the circle in radians
 			double angle = EvacCircle.getAngle(location.getX(), location.getY());
+			//if we're going clockwise we need to subtract d from our angle
+			//otherwise we add it to our angle
 			if (direction.equals(Direction.CW)) angle -= d;
 			else angle += d;
+			//adjust our angle if it goes negative
 			if (angle - EPSILON < 0) angle = 2 * Math.PI + angle;
+			//find our new position based on our angle and subtract d from total distance
 			location = new EvacPoint(Math.sin(angle), Math.cos(angle));
 			distance -= d;
 			break;
+		//if movement mode is not rotate we need to move in a straight line
 		case CIRCUMFERENCE: case EXIT:
+			//get the point that is d behind us on our current line
 			double deltaX = location.getX() - destination.getX();
 			double deltaY = location.getY() - destination.getY();
 			angle = EvacCircle.getAngle(deltaX, deltaY);
 			double newX = location.getX() + Math.sin(angle) * d;
 			double newY = location.getY() + Math.cos(angle) * d;
+			//set location to the new location
 			location = new EvacPoint(newX, newY);
-			distance += d;
+			//decrease total distance traveled by d
+			distance -= d;
 			break;
 		}
 	}
